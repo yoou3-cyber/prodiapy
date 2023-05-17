e4d9dae731b1f337731f202f4575941782600536
Zenafey <118455214+yoou3-cyber@users.noreply.github.com>
2023-05-16 21:31:37 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 html_theme = 'cloud'
 html_static_path = ['_static']
-html_logo = "test.jpg"
+html_logo = "myavatar.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
